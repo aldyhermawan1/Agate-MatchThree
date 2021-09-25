5cc686c928a8e742c6a9b9a3f1ff5f7b179cdb06
Aldy Hermawan <41948718+aldyhermawan1@users.noreply.github.com>
2021-09-25 23:47:07 +0800

    Selecting Tile

--- Assets/Scripts/TileController.cs
+++ Assets/Scripts/TileController.cs
@@ -9,10 +9,45 @@ public class TileController : MonoBehaviour
     private BoardManager board;
     private SpriteRenderer render;
 
+    private static readonly Color selectedColor = new Color(0.5f, 0.5f, 0.5f);
+    private static readonly Color normalColor = Color.white;
+
+    private static TileController previouseSelected = null;
+
+    private bool isSelected = false;
+
     private void Awake()
     {
         board = BoardManager.Instance;
         render = GetComponent<SpriteRenderer>();
+    }
+
+    private void OnMouseDown()
+    {
+        //Non Selectable
+        if (render.sprite == null)
+        {
+            return;
+        }
+
+        //Already selected
+        if (isSelected)
+        {
+            Deselect();
+        }
+        else
+        {
+            //Nothing selected yet
+            if (previouseSelected == null)
+            {
+                Select();
+            }
+            else
+            {
+                previouseSelected.Deselect();
+                Select();
+            }
+        }
     }
 
     public void ChangeId(int id, int x, int y)
@@ -21,5 +56,19 @@ public class TileController : MonoBehaviour
         this.id = id;
 
         name = "TILE_" + id + " (" + x + ", " + y + ")";
+    }
+
+    private void Select()
+    {
+        isSelected = true;
+        render.color = selectedColor;
+        previouseSelected = this;
+    }
+
+    private void Deselect()
+    {
+        isSelected = false;
+        render.color = normalColor;
+        previouseSelected = null;
     }
 }
