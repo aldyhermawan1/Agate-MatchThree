c97057f74b7dccf2e6a80ada927e6f672f6fc450
Aldy Hermawan <41948718+aldyhermawan1@users.noreply.github.com>
2021-09-26 22:16:36 +0800

    GameOver fix

--- Assets/Scripts/UIGameOver.cs
+++ Assets/Scripts/UIGameOver.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class UIGameOver : MonoBehaviour
 {
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex);
+        }
+    }
     public void Show()
     {
         gameObject.SetActive(true);
